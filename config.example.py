a8038e42386ffc64a0412c51da20ce4d2efa75f8
bandaleomug <166202716+dhwlgjs091221@users.noreply.github.com>
2025-06-02 19:51:04 +0900

    Update and rename config.example.py to config.py

--- config.example.py
+++
@@ -1,5 +0,0 @@
-# Configuration example
-SUPABASE_URL = "https://your-project.supabase.co"
-SUPABASE_KEY = "your-anon-key"
-STORAGE_BUCKET = "avatars"
-WEBSOCKET_SERVER_URL = "ws://localhost:8000/ws"
---
+++ config.py
@@ -0,0 +1,5 @@
+# Configuration example
+SUPABASE_URL = "https://csosixcrhclbitkagwjf.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzb3NpeGNyaGNsYml0a2Fnd2pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NDk5ODUsImV4cCI6MjA2NDQyNTk4NX0.JuQgk8RkPlcNV38yEhn9QF-o895l9rIeZNTcnemLdnQ"
+STORAGE_BUCKET = "avatars"
+WEBSOCKET_SERVER_URL = "ws://localhost:8000/ws"
