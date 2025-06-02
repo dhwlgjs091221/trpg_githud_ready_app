513edf17c0d74219019021e837b481003b66ff24
bandaleomug <166202716+dhwlgjs091221@users.noreply.github.com>
2025-06-02 21:30:15 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Configuration example
-SUPABASE_URL = "https://csosixcrhclbitkagwjf.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzb3NpeGNyaGNsYml0a2Fnd2pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NDk5ODUsImV4cCI6MjA2NDQyNTk4NX0.JuQgk8RkPlcNV38yEhn9QF-o895l9rIeZNTcnemLdnQ"
+SUPABASE_URL = "https://dldyccaammfychjyttlw.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsZHljY2FhbW1meWNoanl0dGx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NTg1MzYsImV4cCI6MjA2NDQzNDUzNn0.7CtgpVEF7t-aNKjoys9aSG5dXxhzQcaQm1ngcHlHbgs"
 STORAGE_BUCKET = "avatars"
 WEBSOCKET_SERVER_URL = "ws://localhost:8000/ws"
